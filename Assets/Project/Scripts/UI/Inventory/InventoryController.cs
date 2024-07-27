36aa033127f75f5225280be81c6d16c833470e98
maverick <abazaevpasa@gmail.com>
2024-07-27 22:35:37 +0300

    10

--- Assets/Project/Scripts/UI/Inventory/InventoryController.cs
+++ Assets/Project/Scripts/UI/Inventory/InventoryController.cs
@@ -1,4 +1,6 @@
 using DG.Tweening;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Zenject;
@@ -10,6 +12,8 @@ public class InventoryController : MonoBehaviour
     private Vector2 _slotsShakingDiraction;
     private bool _isInventoryOpen;
 
+    [SerializeField] private TMP_Text _fullInventoryWarning;
+
     [SerializeField] private float _scrollDuration;
     [SerializeField] private float _scrollSensivity;
 
@@ -19,27 +23,42 @@ public class InventoryController : MonoBehaviour
     [Inject]
     private void Construct(PlayerInput input) => _playerInput = input;
 
+    private void Start()
+    {
+        _isInventoryOpen = _slotsPanal.gameObject.activeSelf;
+    }
+
     private void FixedUpdate()
     {
-        Vector2 input = _playerInput.Player.LookInput.ReadValue<Vector2>();
+        if (!_isInventoryOpen) return;
 
+        Vector2 input = _playerInput.Player.LookInput.ReadValue<Vector2>();
         _slotsShakingDiraction = input;
         _slotsPanal.DOLocalMove(_slotsShakingDiraction, 1);
     }
 
     private void CloseOpenPanal(InputAction.CallbackContext context)
     {
-        _isInventoryOpen = _slotsPanal.gameObject.activeSelf;
+        _isInventoryOpen = !_isInventoryOpen;
 
         _slotsPanal.gameObject.SetActive(_isInventoryOpen);
-
         int finalyScale = _isInventoryOpen ? 1 : 0;
 
         _slotsPanal.DOScaleX(finalyScale, 0.1f);
-
     }
 
     private void ScrollSlots(float value) => _slots.DOLocalMoveX(_slots.localPosition.x + _scrollSensivity * value, _scrollDuration);
+
+    private IEnumerator FullInventoryException()
+    {
+        _fullInventoryWarning.DOFade(1, 0.1f);
+
+        _fullInventoryWarning.DOFade(0, 0.1f);
+
+        yield return null;
+
+        StopCoroutine(FullInventoryException());
+    } 
 
     private void OnEnable()
     {
